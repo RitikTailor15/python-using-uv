4e887dab1689e61cb9ff2b307c73c8fcffc3f35f
RitikTailor15 <ritiktailor15@gmail.com>
2026-09-12 15:52:11 +0530

    Practice control flow

--- main.py
+++ main.py
@@ -78,6 +78,97 @@ def main():
     # skills_a.add("fastapi")
     # print(skills_a)
 
+    # users = [
+    #     {"name": "Amit", "age": 25},
+    #     {"name": "Priya", "age": 30},
+    #     {"name": "Ritik", "age": 26},
+    # ]
+
+    # for user in users:
+    #     print(user['name'])
+
+    # Control flow Practice
+
+    # 1. Grade Calculator
+    # score = 82
+
+    # if score >= 90:
+    #     print("A")
+    # elif score >= 75:
+    #     print("B")
+    # elif score >= 60:
+    #     print("C")
+    # else:
+    #     print("D")
+
+    # 2. FizzBuzz
+    # for i in range(1,30):
+    #     if i % 3 == 0 and i % 5 == 0:
+    #         print('FizzBuzz')
+    #     elif i % 3 == 0:
+    #         print("Fizz")
+    #     elif i % 5 == 0:
+    #         print("Buzz")
+    #     else:
+    #         print(i)
+
+    # 3. Sum and Average
+
+    # numbers = [12, 45, 7, 89, 23, 56, 3]
+    # sum  = 0
+    # for num in numbers:
+    #     sum += num
+    # print(f"Total : {sum}, Average : {sum / len(numbers)}")
+
+    # 4. Max in numbers
+
+    # maxNum = numbers[0]
+    # for num in numbers[1:]:
+    #     if num > maxNum:
+    #         maxNum = num
+
+    # print(f"Maximum number in arr : {maxNum}")
+
+    # 5. Enumerator Pratice
+    # languages = ["Python", "JavaScript", "Go", "Rust"]
+    # for idx, str in enumerate(languages):
+    #     print(f"{idx + 1} : {str}")
+
+    # 6. Dictionary loop
+    # inventory = {"apples": 10, "bananas": 0, "mangoes": 5}
+    # for (key, value) in inventory.items():
+    #     if value > 0:
+    #         print(f"{key} : in stock")
+    #     else:
+    #         print(f"{key} is out of stock")
+
+    # 7. While loop function
+    # count  = 10
+    # while count >= 1:
+    #     print(count)
+    #     count -= 1
+
+    # print("Liftoff!!")
+    
+    # 8. Break and continue
+    
+    # numbers = [3, 7, 2, 9, 4, 11, 6, 15, 1]
+
+    # for num in numbers:
+    #     if num > 5:
+    #         continue
+    #     elif num > 15:
+    #         break
+    #     else:
+    #         print(num)
+
+    # 9. Nested loops  = Multiplication Table
+
+    # for i in range(1, 6):
+    #     for j in range(1, 6):
+    #         print(f"{i} x {j} = {i * j}")
+    #     print("-"*15)
+
     users = [
         {"name": "Amit", "age": 25},
         {"name": "Priya", "age": 30},
@@ -85,7 +176,10 @@ def main():
     ]
 
     for user in users:
-        print(user['name'])
+        if user['age'] > 25:
+            print(f"{user['name']} is eligible for the program.")
+        else:
+            print(f"{user['name']} is not eligible for the program.")
 
 if __name__ == "__main__":
     main()
