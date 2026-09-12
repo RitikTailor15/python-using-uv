462adad637b987bad9807b264ab712b576aa3428
RitikTailor15 <ritiktailor15@gmail.com>
2026-09-12 15:20:16 +0530

    Practice String, list, dict, and tuple

--- main.py
+++ main.py
@@ -38,8 +38,54 @@ def main():
     # score = 10
     # score = "ten"
     # print(score)
-    pass
     
+    # sentence = "  Learning Python is fun  "
+    # print(len(sentence.strip(" ").upper().split(" ")))
+
+    # language = ["JS", "RUST", "NODE", "JAVA", "GO"]
+    # print(f"First : {language[0]}")
+    # print(f"last : {language[-1]}")
+    # language.append("PYTHON")
+    # print(language)
+    # print(language[2:4])
+    # print("PYTHON" in language)
+
+    # me = {
+    #     "name": "Ritik",
+    #     "role": "frontend developer",
+    #     "skills": ["React", "JavaScript", "Node.js"]
+    # }
+
+    # me['learning'] = "Python"
+    # print(me)
+    # print(me.get("salary", "Not defined!"))
+    # me['skills'].append("FastAPI")
+    # print(me)
+
+    # profile = ("Ritik", "Vidisha", "Frontend Developer")
+    # name, city, role = profile
+    # print(name, city, role)
+    # # profile[0] = 'X'
+    # print(f"{name} lives in {city} with role {profile}")
+
+    # skills_a = {"python", "react", "sql"}
+    # skills_b = {"react", "docker", "python"}
+
+    # print(skills_a & skills_b)
+    # print(skills_a.intersection(skills_b))
+    # print(skills_a - skills_b)
+    # print(skills_a.difference(skills_b))
+    # skills_a.add("fastapi")
+    # print(skills_a)
+
+    users = [
+        {"name": "Amit", "age": 25},
+        {"name": "Priya", "age": 30},
+        {"name": "Ritik", "age": 26},
+    ]
+
+    for user in users:
+        print(user['name'])
 
 if __name__ == "__main__":
     main()
