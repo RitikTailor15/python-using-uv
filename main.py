ebfa1305dd8216465b40f2a09ce89b7cc941acf4
RitikTailor15 <ritiktailor15@gmail.com>
2026-09-14 16:56:36 +0530

    Practice classes and dataclasses

--- main.py
+++ main.py
@@ -1,65 +1,161 @@
-import json
+class BankAccount:
+    def __init__(self, owner, balance = 0):
+        self.owner = owner
+        self.balance = balance
 
-students = [
-    {"name": "Amit", "scores": [78, 85, 92]},
-    {"name": "Priya", "scores": [55, 60, 45]},
-    {"name": "Ritik", "scores": [88, 91, 95]},
-    {"name": "Neha", "scores": [30, 40, 25]},
-    {"name": "Karan", "scores": [70, 72, 68]},
-]
+    def __str__(self):
+        return f"{self.owner}'s account : ${self.balance}"
 
-def get_average(scores):
-    return sum(scores) / len(scores)
+    def deposit(self, amount):
+        self.balance += amount
 
-def get_grade(avg, strict = False):
-    threshold = 0
-    if strict:
-        threshold = 5
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds")
+        else:
+            self.balance -= amount
+        
 
-    if avg >= (90 + threshold):
-        return "A"
-    elif avg >= (75 + threshold):
-        return "B"
-    elif avg >= (60 + threshold):
-        return "C"
-    elif avg >= (40 + threshold):
-        return "D"
-    else:
-        return "F"
+class Employee:
+    company = "TechCorp"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def __str__(self):
+        return f"{self.name}'s salary is {self.salary} in company {self.company}"
+        
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def speak(self):
+        return f"{self.name} says {self.sound}"
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, 'Woof')
+
+    # def speak(self):
+    #     return "Woof"
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name, 'Meow')
+
+    # def speak(self):
+    #     return "Meow"
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __eq__(self, other):
+#         return self.x == other.x and self.y == other.y
+
+from dataclasses import dataclass
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+class Vehicle:
+    def __init__(self):
+        pass
+
+    def describe(self):
+        return "Vehicle class is the super class"
+
+class Car(Vehicle):
+    def __init__(self):
+        super().__init__()
+
+    def describe(self):
+        return f"Car class is the sub class of {super().describe()}"
+
+class ElectricCar(Car):
+    def __init__(self):
+        super().__init__()
+
+    def describe(self):
+        return f"Electric Car class is the sub class of {super().describe()}"
+    
+class Playlist:
+    def __init__(self, name):
+        self.name = name
+        self.songs = []
+
+    def add_song(self, title):
+        self.songs.append(title)
+
+    def remove_song(self, title):
+        if title in self.songs:
+            self.songs.remove(title)
+        else:
+            print(f"{title} not found in the playlist")
+
+    def __str__(self):
+        return "\n".join(self.songs)
+
+@dataclass
+class Student:
+    name : str
+    scores : list
+
+    def average(self):
+        return  sum(self.scores) / len(self.scores)
+
+    def grade(self):
+        avg = self.average()
+        if avg >= 90:
+            return 'A'
+        elif avg >= 80:
+            return 'B'
+        elif avg >= 70:
+            return 'C'
+        elif avg >= 60:
+            return 'D'
+        else:
+            return 'F'
 
 def main():
+    # account = BankAccount("Ritik")
+    # print(account)
+    # account.deposit(10)
+    # print(account)
+    # account.withdraw(20)
+    # print(account)
+    # account.withdraw(10)
+    # print(account)
 
-    for student in students:
-        # Average score
-        average_score = get_average(student['scores'])
-        student['average'] = round(average_score, 1)
+    # e1 = Employee("Ritik", 10000)
+    # e2 = Employee("Neha", 20000)
+    # print(e1)
+    # print(e2)
 
-        # Grade
-        grade = get_grade(student["average"], True)
-        student['grade'] = grade
+    # d = Dog("Doggo")
+    # c = Cat("cattie")
+    # print(d.speak())
+    # print(c.speak())
 
-        # print formatted
-        # print(f"{student["name"]} -> Avg. {student['average']} Grade: {student["grade"]}")
+    # print(Point(1, 2) == Point(1,2))
 
-    sorted_student = sorted(students, key= lambda student : student['average'], reverse=True)
-    # print(json.dumps(sorted_student, indent=4))
-    topper = sorted_student[0]
-    # print(f"{topper["name"]} : {topper["grade"]}")
+    # c = ElectricCar()
+    # print(c.describe())
 
-    # Get all the student which are failed
+    # p = Playlist('Indian')
+    # p.add_song('First')
+    # p.add_song('Second')
+    # p.add_song('Third')
+    # p.add_song('Fourth')
+    # print(p)
+    # p.remove_song('First')
+    # print(p)
 
-    failed_student  = [student for student in students if student['grade'] == "F"]
-    # print(failed_student)
-
-    all_student_averages = [student['average'] for student in students]
-    lowest_average = min(all_student_averages)
-    highest_average = max(all_student_averages)
-    class_average = round(sum(all_student_averages) / len(all_student_averages), 1)
-
-    # print(f"Class Average : {class_average}, Lowest Average : {lowest_average}, Highest Average : {highest_average}")
-
-    name_and_grade_dic = {student["name"] : student["grade"] for student in students}
-    print(json.dumps(name_and_grade_dic, indent=4))
 
 if __name__ == "__main__":
     main()
